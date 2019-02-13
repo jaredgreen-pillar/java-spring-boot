1d5329e0793bc715011fe4c009aeacf4246f608b
jaredgreen <jgreen2@pillartechnology.com>
2019-02-13 13:49:29 -0600

    Initial build.gradle file and Add HelloWorld main method with tests

---
+++ src/test/java/hello/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package hello;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    public void itHasMainMethod() {
+        String[] args = null;
+        HelloWorld.main(args);
+    }
+}
